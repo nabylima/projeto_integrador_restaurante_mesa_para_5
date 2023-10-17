c04afa308060f742516ce0f76b6073e99f15cb44
Quezia Guilhermina Dias dos Santos <queziagds@gmail.com>
2023-10-17 20:48:58 -0300

    adicionando código css e js a index, na index uma class para img do carrossel

---
+++ script-index.js
@@ -0,0 +1,10 @@
+let index = 0;
+const imagens = document.querySelector('.carousel-img');
+
+function mudarImagem(){
+    imagens[index].classList.remove('ativo');
+    index = (index + 1) % imagens.clientHeight;
+    imagens[index].classList.add('ativo')
+}
+
+setInterval(mudarImagem, 2000);//mudar imagem a cada 2 segundos
