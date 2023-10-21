ca9d16779362dfb9f138d46b4ce8f0802485ee12
Naby Fernandes de Lima <136936685+nabylima@users.noreply.github.com>
2023-10-20 22:31:36 -0300

    Revert "continuando css"

--- script-index.js
+++ script-index.js
@@ -5,7 +5,6 @@ function mudarImagem(){
     imagens[index].classList.remove('ativo');
     index = (index + 1) % imagens.clientHeight;
     imagens[index].classList.add('ativo')
-    
 }
 
 setInterval(mudarImagem, 2000);//mudar imagem a cada 2 segundos
