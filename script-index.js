00d5ad7524d53d3c4dcf6d1bf63e5abc6f617d71
Quezia Guilhermina Dias dos Santos <queziagds@gmail.com>
2023-11-09 18:26:30 -0300

    Add popup na index, inserindo no css estilização e js DOM

--- script-index.js
+++ script-index.js
@@ -9,3 +9,24 @@ function mudarImagem(){
 }
 
 setInterval(mudarImagem, 2000);//mudar imagem a cada 2 segundos
+
+/*function popup() {
+    alert("Descubra as delícias do nosso cardápio e faça seu pedido agora mesmo!");
+}
+
+let close = document.getElementById('close');
+let popup = document.getElementById("popup");
+
+close.addEventListener("click", function() {
+  popup.style.display = 'none';
+});*/
+
+function showModal() {
+    let element = document.getElementById("modal");
+    element.classList.add("show-modal");
+}
+
+function hideModal(){
+    let element = document.getElementById("modal");
+    element.classList.remove("show-modal");
+}
